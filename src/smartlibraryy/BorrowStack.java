86a2c80f37889973e9118b4c46cb36ac9ef3da2c
Muhammad Athallah Raya <raya.mathallah@gmail.com>
2026-06-04 17:27:17 +0800

    added borrow history (BorrowStack)

--- src/smartlibraryy/BorrowStack.java
+++ src/smartlibraryy/BorrowStack.java
@@ -4,10 +4,26 @@
  */
 package smartlibraryy;
 
+import java.util.Stack;
 /**
  *
- * @author Legion
+ * @author raya
  */
 public class BorrowStack {
-    
+    private Stack<Book> stack = new Stack<>();
+
+    public void push(Book b) {
+        stack.push(b);
+    }
+
+    public void show() {
+        if (stack.isEmpty()) {
+            System.out.println("History is empty.");
+        } else {
+            for (int i = stack.size() - 1; i >= 0; i--) {
+                Book b = stack.get(i);
+                System.out.println("[ISBN: " + b.getIsbn() + "] " + b.getTitle());
+            }
+        }
+    }
 }
